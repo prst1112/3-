2de0afd8b8f57c590ecade56a9358a22f9f8c43b
prst1112 <31avtobus1@gmail.com>
2026-08-29 19:55:47 +0300

    Add files via upload

---
+++ states/states.py
@@ -0,0 +1,16 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class DealCreate(StatesGroup):
+    choosing_currency = State()
+    entering_amount = State()
+    entering_description = State()
+
+
+class BuyerPay(StatesGroup):
+    waiting_screenshot = State()
+
+
+class RequisiteAdd(StatesGroup):
+    entering_card_country = State()
+    entering_value = State()
